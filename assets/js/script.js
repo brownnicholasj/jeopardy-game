abf4af61c1a6ead008c37f218b4961e04fda9b14
Bryan Barnes <bryan.barnes12@gmail.com>
2021-04-22 15:36:09 -0500

    Fixed workflow for the local storage and added save/retrieve functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 // This is the first draft of the persistant session object. I organized it for ease of use in iteration, but when we get there we may need to make adjustments.
-const sessionQuestions = {
+let sessionQuestions = {
     category1: {
         valule: {
             1: "",
@@ -135,6 +135,7 @@ const sessionQuestions = {
         }}
 } 
 
+let mathFacts = {fact1: {number: '', text: ''}, fact2: {number: '', text: ''}, fact3: {number: '', text: ''}, fact4: {number: '', text: ''}, fact5: {number: '', text: ''}, fact6: {number: '', text: ''}}
 //Function that gives user the option to reload their last session. The internal code at this point is arbitrary, but I wanted something there for testing the functionality.
 function continueLastGame() {
     $('#testdiv').append('<div id="continue"></div>');
@@ -151,52 +152,58 @@ function continueLastGame() {
 //This will be the function that generates the puzzle area from the data in the question object. Again, the internal code is arbitrary at this point.
 function loadQuestionSpace(choice) {
     if (choice == "confirmNew") {
-        $('#testdiv').empty()
-        $('#testdiv').append('<h1>NEW GAME</h1>')
+        $('#testdiv').empty();
+        $('#testdiv').append('<h1>NEW GAME</h1>');
+        callNewQuestions();
     }
     if (choice == "confirmSave") {
         $('#testdiv').empty()
         $('#testdiv').append('<h1>SAVED GAME</h1>')
+        sessionQuestions = JSON.parse(localStorage.getItem('currentSession'));
     }
 }
 //This function checks local storage for a saved game and returns the option to continue if it exists, or a new game if it doesn't.
 function checkLocalStorage() {
     if (localStorage.getItem('currentSession')) {
         continueLastGame();
-        sessionQuestions = localStorage.getItem(JSON.stringify(sessionQuestions));
-        loadQuestionSpace("confirmSave"); 
     } else {
         callNewQuestions(); //This will be the function that makes the API call. It will contain functions for setting the local object (which will save to local storage).
-        loadQuestionSpace("confirmNew");
+        saveSession();
     }
+}
+// This function saves the function's question object to local storage.
+function saveSession(object) {
+    localStorage.setItem('currentSession', JSON.stringify(object));
 }
 
 //#####################################TESTING ITERATED FETCH CALLS################################
 //testing for the population of the session questions.
-let mathFacts = []
+
 function callNewQuestions() {
+    // Empty array for storing the randomly generated categories.
     let categoryArray = [];
-    for (let i = 0; i < 5; i++) {
-        categoryArray.push(Math.floor(Math.random() * categories.length)); // This is selecting from a list of known, good (data-wise) categories.
+    // Iterating through the possible categories to randomly assign 6. No dublicate testing yet, but we need it.
+    for (let i = 0; i < 6; i++) {
+        categoryArray.push(Math.floor(Math.random() * categories.length));
     }
-    console.log(categoryArray)
-    let promises = [];
+    // Looping fetch call from a number facts API (for testing purposes only) based on the categories that were randomly selected.
     for (let i = 0; i < categoryArray.length; i++) {
+        let keyArray = ['fact1', 'fact2','fact3','fact4','fact5','fact6'];
         (fetch(`http://numbersapi.com/${categoryArray[i]}/math?json`))
             .then(response => response.json())
             .then(function (data) {
-                console.log(data)
-                let tempArray = [];
-                tempArray.push(data.number);
-                tempArray.push(data.text);
-                tempArray.push(data.type);
-                mathFacts.push(tempArray);
+                // Populating the object from which the questions will be rendered. There needs to be a step in between here where we look at questions and values in order to select a whole category. My suspicion is that we'll need to build an array within the scope of this function to contain the data in order to work with it.
+                mathFacts[keyArray[i]].number = data.number;
+                mathFacts[keyArray[i]].text = data.text;
+            })
+            .then(function (data) {
+                // This calls the function that puts our finished category set into local storage.
+                saveSession(mathFacts);
             });
     }
-    console.log(mathFacts);
 }
-callNewQuestions();
-//checkLocalStorage();
+//callNewQuestions();
+checkLocalStorage();
 
 
 
