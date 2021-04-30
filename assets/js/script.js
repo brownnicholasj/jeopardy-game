96433ffa53261940225b98b9cb55a0d028a62aab
brownnicholasj <brown.nicholasj@gmail.com>
2021-04-30 05:47:20 -0500

    added selection logic in the jservice fetch to throw out duplicates, fixed possible timer issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ function checkLocalStorage() {
 	if (localStorage.getItem('currentSession')) {
 		continueLastGame();
 	} else {
-		var currentTime = setInterval(timeBox, 1000);
+		// var currentTime = setInterval(timeBox, 1000);
 		audioSound('startGame');
 		playGame();
 	}
@@ -103,6 +103,7 @@ async function getNewCategory() {
 			.sort((a, b) => a.sort - b.sort)
 			.map((a) => a.value);
 	}
+
 	var newObjectProperty = await organizeData(questionsPull);
 
 	if (newObjectProperty === false) {
@@ -114,12 +115,81 @@ async function getNewCategory() {
 
 async function organizeData(data) {
 	var currentCategoryObject = {};
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < currentCategoryObject.length; i++) {
 		if (!data.clues[i].question || !data.clues[i].answer) {
 			return false;
 		}
 	}
+
 	currentCategoryObject[data.title] = data.clues;
+	// console.log(currentCategoryObject[data.title]);
+	for (var i = 0; i < 5; i++) {
+		if (
+			currentCategoryObject[data.title][0].question ===
+				currentCategoryObject[data.title][i].question &&
+			0 !== i
+		) {
+			// console.log('MATCH 0');
+			// console.log(
+			// 	`${currentCategoryObject[data.title][0].question} (0) vs (${i}) ${
+			// 		currentCategoryObject[data.title][i].question
+			// 	}`
+			// );
+			return false;
+		}
+		if (
+			currentCategoryObject[data.title][1].question ===
+				currentCategoryObject[data.title][i].question &&
+			1 !== i
+		) {
+			// console.log('MATCH 1');
+			// console.log(
+			// 	`${currentCategoryObject[data.title][1].question} (1) vs (${i}) ${
+			// 		currentCategoryObject[data.title][i].question
+			// 	}`
+			// );
+			return false;
+		}
+		if (
+			currentCategoryObject[data.title][2].question ===
+				currentCategoryObject[data.title][i].question &&
+			2 !== i
+		) {
+			// console.log('MATCH 2');
+			// console.log(
+			// 	`${currentCategoryObject[data.title][2].question} (2) vs (${i}) ${
+			// 		currentCategoryObject[data.title][i].question
+			// 	}`
+			// );
+			return false;
+		}
+		if (
+			currentCategoryObject[data.title][3].question ===
+				currentCategoryObject[data.title][i].question &&
+			3 !== i
+		) {
+			// console.log('MATCH 3');
+			// console.log(
+			// 	`${currentCategoryObject[data.title][3].question} (3) vs (${i}) ${
+			// 		currentCategoryObject[data.title][i].question
+			// 	}`
+			// );
+			return false;
+		}
+		if (
+			currentCategoryObject[data.title][4].question ===
+				currentCategoryObject[data.title][i].question &&
+			4 !== i
+		) {
+			// console.log('MATCH 4');
+			// console.log(
+			// 	`${currentCategoryObject[data.title][4].question} (4) vs (${i}) ${
+			// 		currentCategoryObject[data.title][i].question
+			// 	}`
+			// );
+			return false;
+		}
+	}
 	return currentCategoryObject;
 }
 
